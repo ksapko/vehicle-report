66c8c8ff560da07c3e7691a2f63f68610bc081d8
Kasia Sapko <ksapko97@gmail.com>
2022-05-18 09:43:44 +0200

    Initial commit

---
+++ src/main/java/vehiclereport/VehicleReportApplication.java
@@ -0,0 +1,13 @@
+package vehiclereport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleReportApplication.class, args);
+    }
+
+}
